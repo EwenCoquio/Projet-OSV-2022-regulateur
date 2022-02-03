1824f758dd9b5da806b8c69a7776ad7a1a78d878
Ewen Coquio <92462179+EwenCoquio@users.noreply.github.com>
2022-02-03 11:38:22 +0100

    Add files via upload

---
+++ Partie logicielle/Code limiteur/arduino_osv2022_limiteur.ino
@@ -0,0 +1,81 @@
+#include <Arduino.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//definition des pins
+const int accel_pedal = 1; //pedale sur le pin 1
+const int limiteur_state = 34 ; //on place le switch du limiteur sur un pin digital (en bas)
+const int sortie_accel = 7;  //sortie du signal qui va vers le controleur
+
+//definitions des autres variables
+int sw_lim;
+int lim_flag = 0;                 // LED status flag
+
+
+void setup() {
+  Serial.begin(115200);
+  // put your setup code here, to run once:
+  pinMode(accel_pedal, INPUT);
+  pinMode(sortie_accel, OUTPUT);
+  pinMode(limiteur_state, INPUT);
+
+}
+
+int acq_accel() {
+  return analogRead(accel_pedal);
+}
+
+void write_accel(int x) {
+  analogWrite(sortie_accel, x);  //la valeur de x est codé sur 8 bits (255 etant la valeur max)
+}
+
+void write_accel_lim(int x, int y) {
+  map(x, 0, 255 ,0, y);   //y est la valeur de notre vitesse max cap
+  analogWrite(sortie_accel, x);
+}
+
+void loop() {
+  //le limiteur est desactive
+  /*if (analogRead(limiteur_state) >990) {
+    write_accel(acq_accel());
+    Serial.println("********************");
+    Serial.println("limiteur desactive");
+    Serial.print("valeur de la sortie : ");
+    Serial.println(analogRead(sortie_accel));
+    Serial.print("valeur de la course de la pedale : ");
+    Serial.println(analogRead(accel_pedal));
+    }
+    //le limiteur est active
+    else if(analogRead(limiteur_state) <300) {
+    write_accel(100);
+    Serial.println("********************");
+    Serial.println("limiteur active");
+    Serial.print("valeur de la sortie : ");
+    Serial.println(analogRead(sortie_accel));
+    Serial.print("valeur de la course de la pedale : ");
+    Serial.println(analogRead(accel_pedal));
+    }
+  */
+  if (digitalRead(limiteur_state)) {
+  write_accel(acq_accel());
+    Serial.println("********************");
+    Serial.println("limiteur desactive");
+    Serial.print("valeur de la course de la pedale : ");
+    Serial.println(analogRead(accel_pedal));
+    Serial.print("valeur de la sortie : ");
+    Serial.println(analogRead(sortie_accel));
+    
+  }
+  else {
+    write_accel_lim(acq_accel(),125);
+    Serial.println("********************");
+    Serial.println("limiteur active");
+    Serial.print("valeur de la course de la pedale : ");
+    Serial.println(analogRead(accel_pedal));
+    Serial.print("valeur de la sortie : ");
+    Serial.println(analogRead(sortie_accel));
+    
+  }
+  delay(500);
+  
+}
